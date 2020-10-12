1431558b83df57502589cb2b9db4b3aabf959a55
greaka <git@greaka.de>
2020-10-12 19:10:23 +0200

    initial commit

---
+++ src/gw2al_api.rs
@@ -0,0 +1,105 @@
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(unused)]
+
+use std::os::raw::c_void;
+
+pub const GW2AL_CORE_FUNN_HASH_NAME: gw2al_hashed_name = 1;
+pub const GW2AL_CORE_FUNN_REG_FUN: gw2al_hashed_name = 2;
+pub const GW2AL_CORE_FUNN_UNREG_FUN: gw2al_hashed_name = 3;
+pub const GW2AL_CORE_FUNN_QUERY_FUN: gw2al_hashed_name = 4;
+pub const GW2AL_CORE_FUNN_FILL_VTBL: gw2al_hashed_name = 5;
+pub const GW2AL_CORE_FUNN_UNLOAD_ADDON: gw2al_hashed_name = 6;
+pub const GW2AL_CORE_FUNN_LOAD_ADDON: gw2al_hashed_name = 7;
+pub const GW2AL_CORE_FUNN_QUERY_ADDON: gw2al_hashed_name = 8;
+pub const GW2AL_CORE_FUNN_WATCH_EVENT: gw2al_hashed_name = 9;
+pub const GW2AL_CORE_FUNN_UNWATCH_EVENT: gw2al_hashed_name = 10;
+pub const GW2AL_CORE_FUNN_QUERY_EVENT: gw2al_hashed_name = 11;
+pub const GW2AL_CORE_FUNN_TRIGGER_EVENT: gw2al_hashed_name = 12;
+pub const GW2AL_CORE_FUNN_CLIENT_UNLOAD: gw2al_hashed_name = 13;
+pub const GW2AL_CORE_FUNN_LOG_TEXT: gw2al_hashed_name = 14;
+pub const GW2AL_CORE_FUNN_D3DCREATE_HOOK: gw2al_hashed_name = 15;
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct gw2al_addon_dsc {
+    pub name:        *const u16,
+    pub description: *const u16,
+    pub majorVer:    u8,
+    pub minorVer:    u8,
+    pub revision:    u32,
+    pub dependList:  *mut gw2al_addon_dsc,
+}
+
+#[repr(i32)]
+pub enum gw2al_api_ret {
+    OK               = 0,
+    FAIL             = 1,
+    IN_USE           = 2,
+    NOT_FOUND        = 3,
+    BAD_DLL          = 4,
+    DEP_NOT_LOADED   = 5,
+    DEP_OUTDATED     = 6,
+    DEP_STILL_LOADED = 7,
+    STATIC_LIMIT_HIT = 8,
+}
+
+#[repr(i32)]
+pub enum gw2al_log_level {
+    INFO  = 0,
+    ERR   = 1,
+    WARN  = 2,
+    DEBUG = 3,
+}
+
+#[cfg(log)]
+impl From<log::Level> for gw2al_log_level {
+    fn from(level: log::Level) -> Self {
+        use log::Level;
+        match level {
+            Level::Error => Self::ERR,
+            Level::Warn => Self::WARN,
+            Level::Info => Self::INFO,
+            _ => Self::DEBUG,
+        }
+    }
+}
+
+pub type gw2al_api_event_handler = unsafe extern "C" fn(data: *mut c_void);
+pub type gw2al_hashed_name = u64;
+pub type gw2al_event_id = u32;
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct gw2al_core_vtable {
+    pub hash_name:           unsafe extern "C" fn(name: *mut u16) -> u64,
+    pub unregister_function: unsafe extern "C" fn(name: gw2al_hashed_name),
+    pub query_function:      unsafe extern "C" fn(name: gw2al_hashed_name) -> *mut c_void,
+    pub unload_addon:        unsafe extern "C" fn(name: gw2al_hashed_name) -> gw2al_api_ret,
+    pub load_addon:          unsafe extern "C" fn(name: *mut u16) -> gw2al_api_ret,
+    pub query_addon:         unsafe extern "C" fn(name: gw2al_hashed_name) -> *mut gw2al_addon_dsc,
+    pub query_event:         unsafe extern "C" fn(name: gw2al_hashed_name) -> gw2al_event_id,
+    pub trigger_event:       unsafe extern "C" fn(id: gw2al_event_id, data: *mut c_void) -> u32,
+    pub client_unload:       unsafe extern "C" fn(),
+    pub fill_vtable:
+        unsafe extern "C" fn(nameList: *mut gw2al_hashed_name, vtable: *mut *mut c_void),
+    pub unwatch_event: unsafe extern "C" fn(id: gw2al_event_id, subscriber: gw2al_hashed_name),
+    pub watch_event: unsafe extern "C" fn(
+        id: gw2al_event_id,
+        subscriber: gw2al_hashed_name,
+        handler: gw2al_api_event_handler,
+        priority: u32,
+    ) -> gw2al_api_ret,
+    pub register_function:
+        unsafe extern "C" fn(function: *mut c_void, name: gw2al_hashed_name) -> gw2al_api_ret,
+    pub log_text: unsafe extern "C" fn(level: gw2al_log_level, source: *mut u16, text: *mut u16),
+}
+
+// addon must export these functions as
+// gw2addon_get_description
+pub type gw2al_addon_get_dsc_proc = unsafe extern "C" fn() -> *mut gw2al_addon_dsc;
+// gw2addon_load
+pub type gw2al_addon_load_proc =
+    unsafe extern "C" fn(core_api: *mut gw2al_core_vtable) -> gw2al_api_ret;
+// gw2addon_unload
+pub type gw2al_addon_unload_proc = unsafe extern "C" fn(gameExiting: i32) -> gw2al_api_ret;
---
+++ src/lib.rs
@@ -0,0 +1,157 @@
+mod gw2al_api;
+pub use gw2al_api::*;
+#[cfg(log)]
+use log::{Metadata, Record};
+use std::ffi::c_void;
+use widestring::{U16CStr, U16CString};
+
+pub struct Gw2Al<'a> {
+    vtable: &'a gw2al_core_vtable,
+}
+
+impl Gw2Al<'_> {
+    pub fn hash_name(&self, name: &str) -> gw2al_hashed_name {
+        let name = U16CString::from_str(name);
+        let name = if name.is_err() {
+            std::ptr::null_mut()
+        } else {
+            name.unwrap().as_ptr() as _
+        };
+        unsafe { (self.vtable.hash_name)(name) }
+    }
+
+    pub fn register_function(
+        &self,
+        function: *mut c_void,
+        name: gw2al_hashed_name,
+    ) -> gw2al_api_ret {
+        unsafe { (self.vtable.register_function)(function, name) }
+    }
+
+    pub fn unregister_function(&self, name: gw2al_hashed_name) {
+        unsafe { (self.vtable.unregister_function)(name) }
+    }
+
+    pub fn query_function(&self, name: gw2al_hashed_name) -> *mut c_void {
+        unsafe { (self.vtable.query_function)(name) }
+    }
+
+    pub fn fill_vtable(&self, name_list: *mut gw2al_hashed_name, vtable: *mut *mut c_void) {
+        unsafe { (self.vtable.fill_vtable)(name_list, vtable) }
+    }
+
+    pub fn load_addon(&self, name: &str) -> gw2al_api_ret {
+        let name = U16CString::from_str(name);
+        let name = if name.is_err() {
+            std::ptr::null_mut()
+        } else {
+            name.unwrap().as_ptr() as _
+        };
+        unsafe { (self.vtable.load_addon)(name) }
+    }
+
+    pub fn unload_addon(&self, name: gw2al_hashed_name) -> gw2al_api_ret {
+        unsafe { (self.vtable.unload_addon)(name) }
+    }
+
+    pub fn query_addon(&self, name: gw2al_hashed_name) -> Result<Gw2AlAddonDsc, ()> {
+        let dsc = unsafe { (self.vtable.query_addon)(name) };
+        if unsafe { (&*dsc).name }.is_null() {
+            return Err(());
+        }
+        Ok(dsc.into())
+    }
+
+    pub fn watch_event(
+        &self,
+        id: gw2al_event_id,
+        subscriber: gw2al_hashed_name,
+        handler: gw2al_api_event_handler,
+        priority: u32,
+    ) -> gw2al_api_ret {
+        unsafe { (self.vtable.watch_event)(id, subscriber, handler, priority) }
+    }
+
+    pub fn unwatch_event(&self, id: gw2al_event_id, subscriber: gw2al_hashed_name) {
+        unsafe { (self.vtable.unwatch_event)(id, subscriber) }
+    }
+
+    pub fn query_event(&self, name: gw2al_hashed_name) -> gw2al_event_id {
+        unsafe { (self.vtable.query_event)(name) }
+    }
+
+    pub fn trigger_event(&self, id: gw2al_event_id, data: *mut c_void) -> u32 {
+        unsafe { (self.vtable.trigger_event)(id, data) }
+    }
+
+    pub fn client_unload(&self) {
+        unsafe { (self.vtable.client_unload)() }
+    }
+
+    pub fn log_text(&self, level: gw2al_log_level, source: &str, text: &str) {
+        let source = U16CString::from_str(source);
+        let source = if source.is_err() {
+            std::ptr::null_mut()
+        } else {
+            source.unwrap().as_ptr() as _
+        };
+        let text = U16CString::from_str(text);
+        let text = if text.is_err() {
+            std::ptr::null_mut()
+        } else {
+            text.unwrap().as_ptr() as _
+        };
+        unsafe { (self.vtable.log_text)(level, source, text) }
+    }
+}
+
+#[cfg(log)]
+impl log::Log for Gw2Al<'_> {
+    fn enabled(&self, _metadata: &Metadata<'_>) -> bool {
+        true
+    }
+
+    fn log(&self, record: &Record<'_>) {
+        let from = format!(
+            "{}:{}",
+            record.file().unwrap_or_default(),
+            record.line().unwrap_or_default()
+        );
+        self.log_text(record.level().into(), &from, &record.args().to_string());
+    }
+
+    fn flush(&self) {}
+}
+
+pub struct Gw2AlAddonDsc {
+    pub name:            String,
+    pub description:     String,
+    pub version:         (u8, u8, u32),
+    pub dependency_list: Vec<Gw2AlAddonDsc>,
+}
+
+impl From<*mut gw2al_addon_dsc> for Gw2AlAddonDsc {
+    fn from(raw: *mut gw2al_addon_dsc) -> Self {
+        let raw = unsafe { &*raw };
+        let mut i = 0;
+        let mut deps = Vec::new();
+        loop {
+            unsafe {
+                let offset = raw.dependList.offset(i);
+                let obj = &*offset;
+                if obj.name.is_null() {
+                    break;
+                }
+                let safe = offset.into();
+                deps.push(safe);
+                i += 1;
+            }
+        }
+        Self {
+            name:            unsafe { U16CStr::from_ptr_str(raw.name) }.to_string_lossy(),
+            description:     unsafe { U16CStr::from_ptr_str(raw.name) }.to_string_lossy(),
+            version:         (raw.majorVer, raw.minorVer, raw.revision),
+            dependency_list: deps,
+        }
+    }
+}
